408535d64be58e5c01882877e857d400b9cf4430
akankshanehete <akankshanehete8@gmail.com>
2021-12-28 11:44:23 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+# 1. create a snake body
+# 2. Move the snake
+# 3. Control the Snake using keyboard controls
+# 4. detect collision with food
+# 5. create a scoreboard
+# 6. detect collision with wall
+# 7. detect collision with tail
+
+screen = Screen()
