c31a33c58fe72bd1565f6ed4c046471515681712
akankshanehete <akankshanehete8@gmail.com>
2021-12-28 20:53:09 -0500

    finished game

--- main.py
+++ main.py
@@ -52,10 +52,8 @@ while game_is_on:
 
     # detect collision with tail
     # if head collides with any segment in tail, trigger game over sequence
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:-1]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             sb.game_over()
 
